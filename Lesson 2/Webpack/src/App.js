a3521f6b468b43bba3cad07ef967667499452a6d
Valsnei <valsnei@gmail.com>
2022-05-11 10:21:17 -0300

    commit inicial

---
+++ Lesson 2/Webpack/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+Import Button from './Button'
+
+const App = () => {
+    return(
+
+    <div>
+        <p>Digital Innovation One</p>
+        <p>Bem vindo a nossa aula</p>
+        <p>Webpack 4</p>
+        <Button />
+    </div>
+  )
+
+  
+};
+export default App;
